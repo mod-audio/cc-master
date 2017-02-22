fdbfd9eac327d42af9153550aba290f88ee380ad
Ricardo Crudo <ricardo.crudo@gmail.com>
2017-02-22 15:00:28 -0300

    client/cc_client.py: fix parsing multiple messages

--- client/src/cc_client.py
+++ client/src/cc_client.py
@@ -11,6 +11,32 @@ from struct import unpack
 class ControlChainClient(object):
     BUFFER_SIZE = 8*1024
 
+    def _process_event(self, msg):
+        recv = json.loads(msg.decode('utf-8'))
+        if 'event' in recv.keys():
+            if recv['event'] == 'device_status':
+                if self._device_status_cb:
+                    self._device_status_cb(recv['data'])
+            elif recv['event'] == 'data_update':
+                decoded = b64decode(recv['data']['raw_data'])
+                n, raw = decoded[0], decoded[1:]
+                updates = [raw[i:i+5] for i in range(0, len(raw), 5)]
+                update_list = []
+                for update in updates:
+                    a, v = unpack('=Bf', update)
+                    update_list.append({'assignment_id':a, 'value':v})
+
+                updates = recv['data']
+                updates.pop('raw_data')
+                updates['updates'] = update_list
+
+                if self._data_update_cb:
+                    self._data_update_cb(updates)
+
+            return True
+
+        return False
+
     def _reader(self):
         while True:
             read_buffer = self.socket.recv(ControlChainClient.BUFFER_SIZE)
@@ -19,34 +45,15 @@ class ControlChainClient(object):
             if len(read_buffer) == 0:
                 continue
 
-            read_buffer = read_buffer[:-1]
+            read_buffer = [x for x in read_buffer.split(b'\x00') if x]
 
-            if b'"event"' in read_buffer:
-                recv = json.loads(read_buffer.decode('utf-8'))
-                if 'event' in recv.keys():
-                    if recv['event'] == 'device_status':
-                        if self._device_status_cb:
-                            self._device_status_cb(recv['data'])
-                    elif recv['event'] == 'data_update':
-                        decoded = b64decode(recv['data']['raw_data'])
-                        n, raw = decoded[0], decoded[1:]
-                        updates = [raw[i:i+5] for i in range(0, len(raw), 5)]
-                        update_list = []
-                        for update in updates:
-                            a, v = unpack('=Bf', update)
-                            update_list.append({'assignment_id':a, 'value':v})
+            for msg in read_buffer:
+                if b'"event"' in msg:
+                    if self._process_event(msg):
+                        continue
 
-                        updates = recv['data']
-                        updates.pop('raw_data')
-                        updates['updates'] = update_list
-
-                        if self._data_update_cb:
-                            self._data_update_cb(updates)
-
-                    continue
-
-            self.read_buffer = read_buffer.decode('utf-8')
-            self.lock.release()
+                self.reply = json.loads(msg.decode('utf-8'))
+                self.lock.release()
 
     def __init__(self, socket_path):
         self.socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -55,7 +62,7 @@ class ControlChainClient(object):
         self.thread.start()
         self.lock = Lock()
         self.lock.acquire()
-        self.read_buffer = None
+        self.reply = None
         self._device_status_cb = None
         self._data_update_cb = None
 
@@ -65,9 +72,8 @@ class ControlChainClient(object):
         self.socket.send(to_send)
 
         self.lock.acquire()
-        reply = json.loads(self.read_buffer)
-        if reply['reply'] == request_name:
-            return reply['data']
+        if self.reply['reply'] == request_name:
+            return self.reply['data']
 
         return None
 
